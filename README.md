aad491bacd146090ac36aa33778b72d276014268
Adil <adil.khanday@cequitysolutions.com>
2019-12-28 12:51:36 +0530

    First real commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# blogger
-A small self learning project to learn advanced NodeJS, Redis, oAuth and Continuous Integration
+# AdvancedNodeStarter
+Starting project for a course on Advanced Node @ Udemy
---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://adil:Aedel9690@nodeproject-shard-00-00-wuphk.mongodb.net:27017,nodeproject-shard-00-01-wuphk.mongodb.net:27017,nodeproject-shard-00-02-wuphk.mongodb.net:27017/test?ssl=true&replicaSet=NodeProject-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123"
+};
